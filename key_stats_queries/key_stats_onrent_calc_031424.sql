c809ec9a9af545ef7389d1f8cadd90f5eecc33b2
stevecalla <callasteven@gmail.com>
2024-03-15 21:16:34 -0600

    Too many changes

--- key_stats_queries/key_stats_onrent_calc_031424.sql
+++ key_stats_queries/key_stats_onrent_calc_031424.sql
@@ -1,6 +1,9 @@
 -- Switch to the newly created database
 USE ezhire_key_metrics;
 
+-- Drop key_metrics_core_onrent_days if exists
+DROP TABLE IF EXISTS key_metrics_core_onrent_days;
+
 -- DONE
 -- add on-rent by country
 -- return date >= 2023 not the pickup date? & keep calendar the same?
@@ -9,12 +12,14 @@ USE ezhire_key_metrics;
 
 -- TODO
 -- add revenue allocation for booking_charge and booking_charge_with_discount
-
 -- add field for pickup month, year, week, day of month
+
+-- create a table of the output
+-- dynamic statement / javascript to create sql on the fly
 -- car rollup
 
 -- Set parameters
-SET @pickup_date = '2024-01-01';
+SET @pickup_date = '2023-01-01';
 SET @return_date = '2023-01-01';
 SET @status = '%Cancel%';
 SET @uae = 'United Arab Emirates'; -- 1
@@ -31,16 +36,16 @@ SET @gbr = 'United Kingdom'; -- 10
 -- Calculate fleet count
 SET @fleet_count = 2000;
 
+-- Create onrent by segment by day
+CREATE TABLE key_metrics_core_onrent_days
 SELECT
+    NOW() AS created_at,
     ct.calendar_date,
-
-    -- BOOKING COUNT
-    SUM(
-        CASE
-            WHEN ct.calendar_date = km.pickup_date THEN 1
-            ELSE 0
-        END
-    ) AS booking_count,
+    YEAR(ct.calendar_date) AS year,
+    QUARTER(ct.calendar_date) AS quarter,
+    MONTH(ct.calendar_date) AS month,
+    WEEK(ct.calendar_date) AS week,
+    DAY(ct.calendar_date) AS day,
 
     -- TOTAL ON-RENT CALCULATION
     COUNT(km.id) AS days_on_rent_whole_day,
@@ -53,21 +58,6 @@ SELECT
             ELSE 0
         END
     ) AS days_on_rent_fraction,
-
-    -- REVENUE ALLOCATION
-    SUM(
-        CASE
-            WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date THEN booking_charge_aed_per_day
-            ELSE 0
-        END
-    ) AS booking_charge_aed_rev_allocation,
-
-    SUM(
-        CASE
-            WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date THEN booking_charge_less_discount_aed_per_day
-            ELSE 0
-        END
-    ) AS booking_charge_Less_discount_aed_rev_allocation,
     
     -- MARKETPLACE VS DISPATCH
     SUM(
@@ -152,7 +142,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@uae) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_uae',
+    ) AS country_on_rent_uae,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@bhr) THEN km.pickup_fraction_of_day
@@ -160,7 +150,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@bhr) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_bhr',
+    ) AS country_on_rent_bhr,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@sau) THEN km.pickup_fraction_of_day
@@ -168,7 +158,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@sau) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_sau',
+    ) AS country_on_rent_sau,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@qat) THEN km.pickup_fraction_of_day
@@ -176,7 +166,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@qat) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_qat',
+    ) AS country_on_rent_qat,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@kwt) THEN km.pickup_fraction_of_day
@@ -184,7 +174,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@kwt) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_kwt',
+    ) AS country_on_rent_kwt,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@pak) THEN km.pickup_fraction_of_day
@@ -192,7 +182,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@pak) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_pak',
+    ) AS country_on_rent_pak,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@geo) THEN km.pickup_fraction_of_day
@@ -200,7 +190,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@geo) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_geo',
+    ) AS country_on_rent_geo,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@omn) THEN km.pickup_fraction_of_day
@@ -208,7 +198,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@omn) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_omn',
+    ) AS country_on_rent_omn,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@sbr) THEN km.pickup_fraction_of_day
@@ -216,7 +206,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@sbr) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_sbr',
+    ) AS country_on_rent_sbr,
     SUM(
         CASE
             WHEN ct.calendar_date = km.pickup_date AND LOWER(country) LIKE LOWER(@gbr) THEN km.pickup_fraction_of_day
@@ -224,9 +214,7 @@ SELECT
             WHEN ct.calendar_date BETWEEN km.pickup_date AND km.return_date AND LOWER(country) LIKE LOWER(@gbr) THEN 1
             ELSE 0
         END
-    ) AS 'country_on_rent_gbr'
-
-    -- CAR TYPE BREAKOUT
+    ) AS country_on_rent_gbr
 
 FROM
     (SELECT calendar_date FROM calendar_table WHERE calendar_date >= @pickup_date) ct
@@ -234,8 +222,15 @@ INNER JOIN
     -- (SELECT * FROM key_metrics_base WHERE pickup_date >= @pickup_date AND status NOT LIKE @status) km
     (SELECT * FROM key_metrics_base WHERE return_date >= @return_date AND status NOT LIKE @status) km
     ON ct.calendar_date >= km.pickup_date AND ct.calendar_date <= km.return_date
-WHERE km.booking_id = 240667
+-- WHERE km.booking_id = 240667
 GROUP BY
     ct.calendar_date
 ORDER BY
 	ct.calendar_date ASC;
+
+-- Add a new column named 'created_at' to the existing table
+-- ALTER TABLE combined_metrics
+-- ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+-- View the table
+SELECT * FROM key_metrics_core_onrent_days LIMIT 10;
